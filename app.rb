5dbca0b8e17833a835fcf748e8780c4315002128
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-11-12 16:29:07 -0500

    On my 2nd lucky number

--- app.rb
+++ app.rb
@@ -25,3 +25,52 @@ get '/age' do
   last_name = params[:lname]
   erb :age, :locals => {last_name: last_name, first_name: first_name}
 end
+
+post '/age' do
+  first_name = params[:fname]
+  last_name = params[:lname]
+  age = params[:age]
+  redirect '/favoritecolor?fname=' + first_name + '&lname=' + last_name + '&age=' + age
+end
+
+get '/favoritecolor' do
+  first_name = params[:fname]
+  last_name = params[:lname]
+  age = params[:age]
+  erb :favoritecolor, :locals => {first_name: first_name, last_name: last_name, age: age}
+end
+
+post '/favoritecolor' do
+  first_name = params[:fname]
+  last_name = params[:lname]
+  age = params[:age]
+  favoritecolor = params[:favcol]
+  redirect '/firstluckynumber?fname=' + first_name + '&lname=' + last_name + '&age=' + age + '&favcol=' + favoritecolor
+end
+
+get '/firstluckynumber' do
+  first_name = params[:fname]
+  last_name = params[:lname]
+  age = params[:age]
+  favcol = params[:favcol]
+  erb :firstluckynumber, :locals => {first_name: first_name, last_name: last_name, age: age, favcol: favcol}
+end
+
+post '/firstluckynumber' do
+  first_name = params[:fname]
+  last_name = params[:lname]
+  age = params[:age]
+  favcol = params[:favcol]
+  favnum1 = params[:favnum1]
+  redirect 'secondluckynumber?fname=' + first_name + '&lname=' + last_name + '&age=' + age + '&favcol=' + favcol + '&favnum1=' + favnum1
+end
+
+get '/secondluckynumber' do
+  first_name = params[:fname]
+  last_name = params[:lname]
+  age = params[:age]
+  favcol = params[:favcol]
+  favnum1 = params[:favnum1]
+  erb :secondluckynumber, :locals => {first_name: first_name, last_name: last_name, age: age, favcol: favcol, favnum1: favnum1}
+end
+#Age, Fav color, 3 lucky numbers, favorite animal
